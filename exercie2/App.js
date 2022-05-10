a08d6cbacb7efd4562b21b19ed3f194312a50015
andremcqu <mcquade.andre@gmail.com>
2022-05-10 13:40:31 +0200

    As resquested I did the second exercice, I only added 19 charateres

---
+++ exercie2/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, flexDirection: "row"}}>
+    <View style={{flex: 3, backgroundColor: "green" }}/>
+    <View style={{flex: 2, backgroundColor: "red" }}/>
+    <View style={{flex: 1, backgroundColor: "blue" }}/>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
