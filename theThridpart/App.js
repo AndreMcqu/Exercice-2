d5e48c0a94d44974d36e1ad5d9407f22d33a4af5
andremcqu <mcquade.andre@gmail.com>
2022-05-10 15:06:43 +0200

    I made two views

---
+++ theThridpart/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, }}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 3, backgroundColor: "green" }} />
+        <View style={{ flex: 2, backgroundColor: "red" }} />
+        <View style={{ flex: 1, backgroundColor: "blue" }} />
+      </View>
+      <View style={{ flex: 1, }}>
+        <View style={{ flex: 3, backgroundColor: "purple" }} />
+        <View style={{ flex: 3, backgroundColor: "yellow" }} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
